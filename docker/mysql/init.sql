7b8fb29df413ae9949491a1092cd24499125bcb8
shini161 <shini161os@gmail.com>
2026-03-28 10:21:53 +0100

    Created database senteur

---
+++ docker/mysql/init.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS senteur;
+USE senteur;
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ======================
+-- SCHEMA
+-- ======================
+
+-- ======================
+-- INDEXES
+-- ======================
+
+-- ======================
+
+SET FOREIGN_KEY_CHECKS = 1;
