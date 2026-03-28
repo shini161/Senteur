a90bdef336d79493674c7113aeaf9a5e15687dec
shini161 <shini161os@gmail.com>
2026-03-28 10:26:30 +0100

    Created table product_categories

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -97,6 +97,16 @@ CREATE TABLE notes (
     image_url VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE product_categories (
+    product_id INT NOT NULL,
+    category_id INT NOT NULL,
+
+    PRIMARY KEY (product_id, category_id),
+
+    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
 -- ======================
 -- INDEXES
 -- ======================
