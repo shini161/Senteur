fd464e1e6768988595eb354a2927122b0d111475
shini161 <shini161os@gmail.com>
2026-03-28 10:33:17 +0100

    Addded order_items db indexes

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -269,6 +269,8 @@ CREATE INDEX idx_orders_user_id ON orders(user_id);
 CREATE INDEX idx_orders_status ON orders(status);
 CREATE INDEX idx_orders_address_id ON orders(shipping_address_id);
 
+CREATE INDEX idx_order_items_order_id ON order_items(order_id);
+
 -- ======================
 
 SET FOREIGN_KEY_CHECKS = 1;
