2f95af23d4b6d7ca95f8c865114e6f11beb3e926
shini161 <shini161os@gmail.com>
2026-03-28 10:23:45 +0100

    Created table users

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -18,6 +18,23 @@ CREATE TABLE fragrance_types (
     name VARCHAR(50) UNIQUE NOT NULL
 );
 
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+
+    public_id CHAR(10) UNIQUE NOT NULL,
+
+    role ENUM('admin', 'user') NOT NULL DEFAULT 'user',
+
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    phone VARCHAR(20) NULL,
+    password_hash TEXT NOT NULL,
+
+    email_verified_at TIMESTAMP NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
 -- ======================
 -- INDEXES
 -- ======================
